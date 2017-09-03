ae2a04e9fcbca26502645abecec8b81072df28a1
Sebastien Le Maguer <slemaguer@coli.uni-saarland.de>
2017-09-03 21:18:24 +0200

    Make emacs package complient

--- cue-mode.el
+++ cue-mode.el
@@ -1,32 +1,29 @@
-;;; cue-mode.el ---
+;;; cue-mode.el ---   -*- lexical-binding: t; coding: utf-8 -*-
 
-;; Copyright 2015 Sébastien Le Maguer
+;; Copyright (C)  3 September 2017
 ;;
-;; Author: Sébastien Le Maguer
-;; Version: $Id: cue-mode.el,v 0.0 2015/09/23 13:19:39 slemaguer Exp $
-;; Keywords:
-;; X-URL: not distributed yet
 
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; Author: Sébastien Le Maguer <slemaguer@coli.uni-saarland.de>
+
+;; Package-Requires: ((emacs "25.2"))
+;; Keywords:
+;; Homepage: https://github.com/seblemaguer/cue-mode
+
+;; cue-mode is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; cue-mode is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;; along with cue-mode.  If not, see http://www.gnu.org/licenses.
 
 ;;; Commentary:
 
-;;
-
-;; Put this file into your load-path and the following into your ~/.emacs:
-;;   (require 'cue-mode)
 
 ;;; Code:
 
